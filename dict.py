41c3df98a637aad58d8b4f19231cc4523d64dea2
Roman <romano@elementor.com>
2025-09-30 09:25:38 +0300

    dicts

--- dict.py
+++ dict.py
@@ -1,3 +1,5 @@
+from itertools import islice
+
 my_dict = {"Israel": "Jerusalem", "USA": "Vashngton D.C.", "Italy": "Rome"}
 print(my_dict)
 first_key = list(my_dict.keys())[0]
@@ -6,5 +8,14 @@ print(f"{first_key} is {first_val}")
 
 grades = {"Alice": 85, "Bob": 92, "Charlie": 78}
 grades["Dav"] = 100
+grades["Dav"] = 101
 print(grades)
 
+print(grades.keys())
+print(grades.values())
+print(grades.items())
+
+item1 = grades["Bob"]
+print(item1)
+second_item = next(islice(grades.values(), 2, None))
+print(second_item)
