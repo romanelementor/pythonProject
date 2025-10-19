27ff200fe6f816b2e2aa9910880609c2f532e3ee
Roman <romano@elementor.com>
2025-10-19 11:40:16 +0300

    oop2

--- oop.py
+++ oop.py
@@ -20,7 +20,7 @@ car_1.display_info()
 car_2.display_info()
 
 
-# t2 BankAccount
+# t2 Encapsulation
 class BankAccount:
     def __init__(self):
         self._balance = 0
@@ -40,4 +40,49 @@ b1.withdraw(200)
 b1.get_balance()
 
 
-# t3
+# t3 Multiple Inheritance
+class Flyable:
+    def fly(self):
+        print("Flying high!")
+
+class Swimmable:
+    def swim(self):  
+        print("Swimming fast!")
+
+class Duck(Flyable, Swimmable):
+    def make_sound(self):
+        print("Quack!")
+
+a = Flyable()
+b = Swimmable()
+c = Duck()
+
+c.fly()
+c.swim()
+c.make_sound()
+
+
+# t4 Polymorphism
+class Cat:
+    def sound(self):
+        return "meuooo !"
+
+class Dog:
+    def sound(self):
+        return "hoo hoo !"
+
+class Cow:
+    def sound(self):
+        return "moo !"
+
+list_obj = []
+
+list_obj.append(Cat())
+list_obj.append(Dog())
+list_obj.append(Cow())
+
+for obj in list_obj:
+    print(obj.sound())
+
+
+# t5 
