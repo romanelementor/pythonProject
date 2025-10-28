518c6250681efbf3da5ccb486117bf3b5ec5c220
Roman <romano@elementor.com>
2025-10-28 20:11:25 +0200

    shop

--- opp_prj.py
+++ opp_prj.py
@@ -3,10 +3,10 @@ from abc import ABC, abstractmethod
 import datetime
 
 class Product:
-    def __init__(self, name, price, quantity, category):
+    def __init__(self, name, price, stock, category):
         self._name = name
         self._price = price
-        self._quantity = quantity
+        self._stock = stock
         self._category = category
 
     @property
@@ -22,8 +22,8 @@ class Product:
         return self._category
 
     @property
-    def get_quantity(self):
-        return self._quantity
+    def get_stock(self):
+        return self._stock
 
     @price.setter
     def price(self, value):
@@ -37,27 +37,9 @@ class Product:
     def quantity(self, value):
         self._quantity = value
 
-class Order:
-    def __init__(self, order_id, products, customer, order_date):
-        self._order_id = order_id
-        self._products = products
-        self._customer = customer
-        self._order_date = order_date
+    def __str__(self) -> str:
+        return f"Product data: {self._name}, price: {self._price}, stock: {self._stock}, category: {self._category}"
 
-    def get_order_id(self):
-        return self._order_id
-
-    def get_products(self):
-        return self._products
-
-    def get_customer(self):
-        return self._customer
-
-    def get_order_date(self):
-        return self._order_date
-
-    def add_product(self, product):
-        self._products.append(product)
 
 class Customer:
     def __init__(self, name, email, address):
@@ -81,3 +63,65 @@ class Customer:
     def address(self, value):
         self._address = value
 
+    def __str__(self) -> str:
+        return f"Customer data: {self._name}, {self._email}, {self._address}"
+
+
+class CartItem:
+    def __init__(self, product, quantity):
+        self._product = product
+        self._quantity = quantity
+
+    def get_product(self):
+        return self._product
+
+    def get_quantity(self):
+        return self._quantity
+
+    def get_total(self):
+        return self.product.price * self.quantity
+
+    def __str__(self) -> str:
+        return f"CartItem data: {self._product}, {self._quantity}"
+
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, cart_item):
+        self.items.append(cart_item)
+
+    def remove_item(self, cart_item):
+        self.items.remove(cart_item)
+
+    def get_total(self):
+        total = 0
+        for item in self.items:
+            total += item.get_total()
+        return total
+
+    def __str__(self) -> str:
+        return f"ShoppingCart data: {self.items} total: {self.get_total()}"
+
+
+class Order:
+    def __init__(self, order_id, products, customer, order_date):
+        self._order_id = order_id
+        self._products = products
+        self._customer = customer
+        self._order_date = order_date
+
+    def get_order_id(self):
+        return self._order_id
+
+    def get_products(self):
+        return self._products
+
+    def get_customer(self):
+        return self._customer
+
+    def get_order_date(self):
+        return self._order_date
+
+    def add_product(self, product):
+        self._products.append(product)
