b6a984dba436608dce93d8e945fc36f2a2cf12b5
Roman <romano@elementor.com>
2025-11-09 20:53:27 +0200

    sda

--- time_process.py
+++ time_process.py
@@ -18,3 +18,28 @@ def sum_numbers():
     return total
 
 print(sum_numbers())
+
+
+# define func in another function
+def outer_func():
+    print("this is outer function")
+    def inner_func():
+        print("this is inner function")
+        return "This is return of inner func"
+    return inner_func
+
+outer_func_my = outer_func
+print(outer_func_my)
+
+inner_func_my = outer_func()
+print(inner_func_my)
+
+# func calls another func
+def foo():
+    print("this is foo function")
+
+def bar(foo_func):
+    print(f"this is bar function and {foo_func.__name__} is called")
+    foo_func()
+
+print(bar(foo))
